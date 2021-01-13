c12ef1c54c2c2704b02ee949f13a1a979178a3d9
Unknown <robert.andersen@uoit.net>
2021-01-13 16:40:44 -0500

    Screen flashes when switching from light to shadow mode and back. Flashes black when switching to shadow and white when switching back.

---
+++ Assets/_Scripts/ChangeForm.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeForm : MonoBehaviour
+{
+    public GameObject flashImage;
+    public bool shadowForm = false;
+    public KeyCode shadowModeKey;
+
+    public float flashTime;
+    public float flashMaxAlpha;
+    public Color flashColor = Color.white;
+    public Color shadowFlashColor = Color.black;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(shadowModeKey))
+        {
+            if (shadowForm == false)
+            {
+                //screen flash black
+                flashImage.GetComponent<ScreenFlash>().StartScreenFlash(flashTime, flashMaxAlpha, shadowFlashColor);
+                //Change sprite to shadow player
+                shadowForm = true;
+            }
+            else if(shadowForm == true)
+            {
+                flashImage.GetComponent<ScreenFlash>().StartScreenFlash(flashTime, flashMaxAlpha, flashColor);
+                //Change sprite to normal player
+                shadowForm = false;
+            }
+        }
+                
+    }
+}
+
---
+++ Assets/_Scripts/ScreenFlash.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ScreenFlash : MonoBehaviour
+{
+    Image image = null;
+    Coroutine currentFlashCo;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+ 
+    public void StartScreenFlash(float seconds, float maxAlpha, Color newColor)
+    {
+        image.color = newColor;
+
+        maxAlpha = Mathf.Clamp(maxAlpha, 0, 1);
+
+        if(currentFlashCo != null)
+        {
+            StopCoroutine(currentFlashCo);
+        }
+        currentFlashCo = StartCoroutine(Flash(seconds, maxAlpha));
+    }
+
+    IEnumerator Flash(float seconds, float maxAlpha)
+    {
+        float flashInDuraction = seconds / 2;
+        for (float i = 0; i <= flashInDuraction; i += Time.deltaTime)
+        {
+            Color tempColor = image.color;
+            tempColor.a = Mathf.Lerp(0, maxAlpha, i / flashInDuraction);
+            image.color = tempColor;
+            yield return null;
+        }
+
+        float flashOutDuration = seconds / 2;
+        for (float i = 0; i <= flashOutDuration; i += Time.deltaTime)
+        {
+            Color tempColor = image.color;
+            tempColor.a = Mathf.Lerp(maxAlpha, 0, i / flashOutDuration);
+            image.color = tempColor;
+            yield return null;
+        }
+
+    }
+}
