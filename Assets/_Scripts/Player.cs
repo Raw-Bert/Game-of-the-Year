7894f766b44ee47b04a077dcd8352d5b4cb9c0ae
Unknown <robert.andersen@uoit.net>
2021-01-19 16:17:53 -0500

    Worlds sickest health bar. When the player collides with something that has the "Hirtful" tag, player takes 20 damage. Player has max 100 health at the moment.

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        if (col.gameObject.tag == "Hurtful")
+        {
+            Debug.Log("printoooo");
+            this.TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+
+}
