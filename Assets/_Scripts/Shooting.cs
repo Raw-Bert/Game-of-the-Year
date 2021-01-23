da832e3f9b4201fe6e6e202d812da63907499fc7
Unknown <robert.andersen@uoit.net>
2021-01-23 01:19:37 -0500

    Player Shooting kinda works, bullets dont have a force tho for some reason, also needs lots of polish :construction:

---
+++ Assets/_Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject basicBullet;
+
+    public float bulletForce;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+ 
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(basicBullet, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = basicBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
