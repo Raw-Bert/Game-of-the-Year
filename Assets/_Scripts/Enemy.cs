79885d5d273d7e526c126615af2b19f4abacaae4
Unknown <robert.andersen@uoit.net>
2021-01-25 16:19:10 -0500

    Enemies have health bars <-- This took way to long lol

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -7,27 +7,30 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 50;
     public int currentHealth;
     GameObject manager;
+
+    public HealthBar healthBar;
     
 
     // Start is called before the first frame update
     void Awake()
     {
-        currentHealth = maxHealth;
         manager = GameObject.Find("LevelController");
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
-    }
+    //void Update(){
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    TakeDamage(20);
+       //}
+    //}
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if(col.gameObject.tag == "Player Bullet")
         {
-            TakeDamage(25);
+            TakeDamage(15);
 
             Destroy(col.gameObject);
         }
@@ -36,9 +39,10 @@ public class Enemy : MonoBehaviour
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
         if(currentHealth <= 0)
         {
-            manager.GetComponent<GameScore>().countScore(10);
+            
             Die();
         }
     }
@@ -46,6 +50,7 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         //Play Enemy death animation here
+        manager.GetComponent<GameScore>().countScore(10);
         Destroy(this.gameObject);
     } 
 }
