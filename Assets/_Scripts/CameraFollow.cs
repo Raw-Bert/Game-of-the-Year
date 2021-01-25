9b8aac44f57e77b6072505b82895996a3f0bb128
Unknown <robert.andersen@uoit.net>
2021-01-24 20:13:38 -0500

    Added Cinemachine package and a shitty cursor

--- Assets/_Scripts/CameraFollow.cs
+++ Assets/_Scripts/CameraFollow.cs
@@ -6,10 +6,16 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public Camera camera;
-    //public Transform targetTransform;
+
+   // public float maxPositionX = 30.0f;
+    //public float minPositionX = -30.0f;
+    //public float maxPositionY = 30.0f;
+    //public float minPositionY = -30.0f;
+
     
     void Update() {
         Debug.Log("run dammit");
+        
     }
     void FixedUpdate() 
     {
@@ -17,7 +23,11 @@ public class CameraFollow : MonoBehaviour
     }
     void LateUpdate()
     {
+        
         camera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        //camera.transform.position.x = Math.Clamp(camera.transform.position.x, minPositionX, maxPositionX);
+        //camera.transform.position.y = Math.Clamp(camera.transform.position.y, minPositionY, maxPositionY);
+        
         Debug.Log("LateUpdate");
     }
 }
