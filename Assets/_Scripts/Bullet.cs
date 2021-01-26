7d0f26b805594ab2ff857d4722340caa16c1c682
Josh <joshua.sankarlal@ontariotechu.net>
2021-01-26 01:14:46 -0500

    Fixed shooting + camera

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -6,13 +6,16 @@ public class Bullet : MonoBehaviour
 {
     GameObject manager;
     Vector2 shootDir;
+    public GameObject mainCamera;
 
     public float speed = 5.0f;
     // Start is called before the first frame update
+
     void Awake()
     {
-        manager = GameObject.Find("LevelController");
-        shootDir = manager.GetComponent<CameraOffset>().CaptureMousePos();
+        //manager = GameObject.Find("LevelController");
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        shootDir = mainCamera.GetComponent<CameraOffset>().CaptureMousePos();
         shootDir.Normalize();
     }
 
