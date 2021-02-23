c8a89eaef51378848ea8f9013e4a2468473b4fd2
Unknown <robert.andersen@uoit.net>
2021-02-23 16:38:22 -0500

    Gun moves towards cursor slightly. And Gun no longer lags when moving with player.

--- Assets/_Scripts/GunRotation.cs
+++ Assets/_Scripts/GunRotation.cs
@@ -9,7 +9,9 @@ public class GunRotation : MonoBehaviour
     public Camera cam;
     //public int sortingOrder = 3;
     public GameObject player;
-    public Vector2 factor = new Vector3(0.01f,0.01f);
+    public Vector3 factor = new Vector3(0.5f,0.5f,0.0f);
+    public Vector3 weaponOffset = new Vector3(0.0f, -0.04f, 0.0f);
+    public float max = 0.9f;
     
     // Start is called before the first frame update
     void Awake()
@@ -21,21 +23,24 @@ public class GunRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //var mousePos = Input.mousePosition;
-        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //Set gun position to player position plus offset. This is to prevent the gun object from lagging behind the parent of the player
+        
+        mousePosition = CaptureMousePos();
+        this.transform.position = player.transform.position + weaponOffset + (new Vector3(factor.x * mousePosition.x, factor.y * mousePosition.y, 0.0f));
         Debug.Log(mousePosition);
-        //Debug.Log("FLIP U LITTLE SHIT");
-        if(mousePosition.x < player.transform.position.x)
+        Debug.Log(new Vector3(factor.x * mousePosition.x, factor.y * mousePosition.y, 0.0f));
+
+        //Flip sprite if mouse past certain axis in relation to the player.
+        if(mousePosition.x < 0.0f)
         {
-            //Debug.Log("FLIP U LITTLE SHIT");
             gunRenderer.flipY = true;
         }
-        else if(mousePosition.x >= player.transform.position.x){
+        else if(mousePosition.x >= 0.0f){
             gunRenderer.flipY = false;
         }
         
-        
-        if (mousePosition.y > player.transform.position.y)
+        //Put gun in layer behind the player depending on mouse position in relation to player
+        if (mousePosition.y > 0.0f)
         {
             gunRenderer.sortingOrder = 1;
         }
@@ -43,5 +48,20 @@ public class GunRotation : MonoBehaviour
 
         //this.gameObject.transform.position.x = this.gameObject.transform.position.x + (mousePosition.x * factor);
         //this.transform.position.y = this.transform.position.y + (mousePosition.y * factor);
+    }
+
+    public Vector2 CaptureMousePos()
+    {
+        Vector2 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        pos *= 2;
+        pos -= Vector2.one;
+
+        if (Mathf.Abs(pos.x) > max || Mathf.Abs(pos.y) > max)
+        {
+            pos = pos.normalized;
+        }
+        //Debug.Log("POS: " + pos);
+
+        return pos;
     }
 }
