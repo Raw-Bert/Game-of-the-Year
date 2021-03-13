649a049f9f24c6adf9260888a8fc5a7d0a12ec92
Safa <safa.nazir@uoit.net>
2021-03-13 16:13:23 -0500

    Updated UI. health bar fill is broken tho

--- Assets/_Scripts/HealthBar.cs
+++ Assets/_Scripts/HealthBar.cs
@@ -7,7 +7,7 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
-    public Gradient gradient;
+    //public Gradient gradient;
     public Image fill;
     
     public void SetMaxHealth(int health)
@@ -15,12 +15,12 @@ public class HealthBar : MonoBehaviour
         slider.maxValue = health;
         slider.value = health;
 
-        fill.color = gradient.Evaluate(1f);
+        //fill.color = gradient.Evaluate(1f);
     }
     public void SetHealth(int health)
     {
         slider.value = health;
 
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        //fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
