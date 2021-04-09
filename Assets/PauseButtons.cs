a2b8fc860950369d4509553b504d7921014d71d7
Emory <emorywynn@gmail.com>
2021-04-09 08:57:56 -0400

    It works, It just works.

---
+++ Assets/PauseButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButtons : MonoBehaviour
+{
+    public void resumeScene()
+    {
+        Time.timeScale = 1;
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void restartScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void toMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
