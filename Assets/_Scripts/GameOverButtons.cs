5109ca116f5839a35249404ed1a166f6f3202f35
Lillian <lillianfan@live.cn>
2021-04-09 04:18:24 -0400

    Fix merge issue, finished game over screen.

--- Assets/_Scripts/GameOverButtons.cs
+++ Assets/_Scripts/GameOverButtons.cs
@@ -5,12 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class GameOverButtons : MonoBehaviour
 {
+    public GameObject selectSymbol;
+    public GameObject upLocation;
+    public GameObject downLocation;
 
+    Vector2 mousePosition;
+
+    private void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
+        if (mousePos.y > 0)
+        {
+            selectSymbol.transform.position = upLocation.transform.position;
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("Level 3 Scene");
+            }
+        }
+        else
+        {
+            selectSymbol.transform.position = downLocation.transform.position;
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("MenuScene");
+            }
+        }
+    }
 
     public void RestartButton()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("_GameScene");
+        SceneManager.LoadScene("Level 3 Scene");
     }
 
     public void MenuButton()
