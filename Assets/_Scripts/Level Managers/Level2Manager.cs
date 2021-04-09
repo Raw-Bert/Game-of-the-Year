7a85705ae0a1ed681be21c0d7fa93783d0658997
Unknown <robert.andersen@uoit.net>
2021-04-09 02:42:18 -0400

    Fixed Josh's Tilemap for the most part. Made sure the 3 levels link together

---
+++ Assets/_Scripts/Level Managers/Level2Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Manager : MonoBehaviour
+{
+    void Awake(){
+        Time.timeScale  = 1;
+    }
+}
--- Assets/_Scripts/Level Managers/SlimeBossLevelManager.cs
+++ Assets/_Scripts/Level Managers/SlimeBossLevelManager.cs
@@ -14,10 +14,9 @@ public class SlimeBossLevelManager : MonoBehaviour
 
     public bool wallsDrop = false;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
